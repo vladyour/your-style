14f41014f4f47a72fd05e67ee21d2084335118a8
vyurevich <vyurevich@okko.tv>
2020-08-17 19:03:40 +0300

    1) Improve Card Component 2) Update version

--- projects/your-style/src/lib/layout/card/card.component.ts
+++ projects/your-style/src/lib/layout/card/card.component.ts
@@ -1,17 +1,33 @@
-import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, Renderer2} from '@angular/core';
 import {StyleBase} from "../../core/style-base";
 
 @Component({
   selector: 'your-card',
-  templateUrl: './card.component.html'
+  templateUrl: './card.component.html',
+  styleUrls: ['card.component.scss']
 })
 export class CardComponent extends StyleBase implements OnInit {
+
+  @Input()
+  display: 'block' | 'flex' = 'flex';
+
+  @Input()
+  inline: boolean = true;
 
   constructor(el: ElementRef, renderer: Renderer2) {
     super(el, renderer);
   }
 
   ngOnInit(): void {
+    let display = '';
+
+    if (this.inline) {
+      display += 'inline-';
+    }
+
+    display += this.display;
+
+    this.renderer.addClass(this.el.nativeElement, display);
   }
 
   getTagPrefix(): string {
